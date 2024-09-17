611cdf6ba79a9fa0269a0580f0ba85c189f7f7ff
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2024-09-17 14:14:24 -0300

    add: card and bird-watchers end

---
+++ bird-watcher/bird_watcher.go
@@ -0,0 +1,28 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	birdTotal := 0
+	for _, bird := range birdsPerDay {
+		birdTotal += bird
+	}
+	return birdTotal
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	weekStartAt := (week - 1) * 7
+	weekEndAt := weekStartAt + 7
+	return TotalBirdCount(birdsPerDay[weekStartAt:weekEndAt])
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i] += 1
+	}
+	return birdsPerDay
+}
